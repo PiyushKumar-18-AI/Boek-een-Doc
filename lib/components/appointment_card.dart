406871c8188d92c0d9d120b9d7b0e8f124e0390d
Piyush Kumar <piyush.kumar.0000018@gmail.com>
2023-02-15 19:29:21 +0530

    Application Updated

--- lib/components/appointment_card.dart
+++ lib/components/appointment_card.dart
@@ -1,3 +1,4 @@
+import 'package:boek_een_doc/utils/config.dart';
 import 'package:flutter/material.dart';
 
 class AppointmentCard extends StatefulWidget {
@@ -10,6 +11,129 @@ class AppointmentCard extends StatefulWidget {
 class _AppointmentCardState extends State<AppointmentCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Container(
+      width: double.infinity,
+      decoration: BoxDecoration(
+          color: Config.primaryColor, borderRadius: BorderRadius.circular(10)),
+      child: Material(
+        color: Colors.transparent,
+        child: Padding(
+          padding: EdgeInsets.all(20),
+          child: Column(children: <Widget>[
+            Row(
+              children: [
+                const CircleAvatar(
+                  radius: 30,
+                  backgroundImage: AssetImage('assets/doctor_1.jpg'),
+                ),
+                const SizedBox(
+                  width: 10,
+                ),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: const <Widget>[
+                    Text(
+                      'Dr. Richard Tan',
+                      style: TextStyle(color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: 10,
+                    ),
+                    Text(
+                      'Dental',
+                      style: TextStyle(color: Colors.black),
+                    )
+                  ],
+                )
+              ],
+            ),
+            Config.spaceSmall,
+            const ScheduleCard(),
+            Config.spaceSmall,
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Expanded(
+                    child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(backgroundColor: Colors.red),
+                  child: const Text(
+                    'Cancel',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                )),
+                SizedBox(
+                  width: 20,
+                ),
+                Expanded(
+                    child: ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  child: const Text(
+                    'Confirm',
+                    style: TextStyle(color: Colors.white),
+                  ),
+                  onPressed: () {},
+                ))
+              ],
+            )
+          ]),
+        ),
+      ),
+    );
+  }
+}
+
+class ScheduleCard extends StatelessWidget {
+  const ScheduleCard({
+    Key? key,
+    /*required this.appointment*/
+  }) : super(key: key);
+  // final Map<String, dynamic> appointment;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        color: Colors.grey,
+        borderRadius: BorderRadius.circular(10),
+      ),
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: const <Widget>[
+          Icon(
+            Icons.calendar_today,
+            color: Colors.white,
+            size: 15,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Text(
+            'Monday, 23/02/2023',
+            style: TextStyle(color: Colors.white),
+          ),
+          SizedBox(
+            width: 20,
+          ),
+          Icon(
+            Icons.access_alarm,
+            color: Colors.white,
+            size: 17,
+          ),
+          SizedBox(
+            width: 5,
+          ),
+          Flexible(
+              child: Text(
+            '2:00 PM',
+            style: TextStyle(color: Colors.white),
+          ))
+        ],
+      ),
+    );
   }
 }
--- lib/components/doctor_card.dart
+++ lib/components/doctor_card.dart
@@ -12,6 +12,70 @@ class DoctorCard extends StatefulWidget {
 class _DoctorCardState extends State<DoctorCard> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    Config().init(context);
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+      height: 150,
+      child: GestureDetector(
+        child: Card(
+          elevation: 5,
+          color: Colors.white,
+          child: Row(
+            children: [
+              SizedBox(
+                width: Config.screenWidth! * 0.33,
+                child: Image.asset(
+                  'assets/doctor_2.jpg',
+                  fit: BoxFit.fill,
+                ),
+              ),
+              Flexible(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    const Text(
+                      'Dr. MC Stan',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                    ),
+                    const Text(
+                      'Dental',
+                      style: TextStyle(
+                          fontSize: 14, fontWeight: FontWeight.normal),
+                    ),
+                    const Spacer(),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: const <Widget>[
+                        Icon(
+                          Icons.star_border,
+                          color: Colors.yellow,
+                          size: 16,
+                        ),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Text('4.5'),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Text('Reviews'),
+                        Spacer(
+                          flex: 1,
+                        ),
+                        Text('(20)')
+                      ],
+                    ),
+                  ],
+                ),
+              ))
+            ],
+          ),
+        ),
+        onTap: () {},
+      ),
+    );
   }
 }
