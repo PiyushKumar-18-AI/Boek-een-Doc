b8813eba26bcbe06fc417dc25f9625148b561c54
Piyush Kumar <piyush.kumar.0000018@gmail.com>
2023-02-15 13:39:39 +0530

    Setup and Login Page

---
+++ android/app/src/main/kotlin/com/example/boek_een_doc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.boek_een_doc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:boek_een_doc/main_layout.dart';
+import 'package:boek_een_doc/screens/auth_page.dart';
+import 'package:boek_een_doc/utils/config.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Boek-een-Doc',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        //pre-define input decoration
+        inputDecorationTheme: const InputDecorationTheme(
+          focusColor: Config.primaryColor,
+          border: Config.outlinedBorder,
+          focusedBorder: Config.focusBorder,
+          errorBorder: Config.errorBorder,
+          enabledBorder: Config.outlinedBorder,
+          floatingLabelStyle: TextStyle(color: Config.primaryColor),
+          prefixIconColor: Colors.black38,
+        ),
+        scaffoldBackgroundColor: Colors.white,
+        bottomNavigationBarTheme: BottomNavigationBarThemeData(
+          backgroundColor: Config.primaryColor,
+          selectedItemColor: Colors.white,
+          showSelectedLabels: true,
+          showUnselectedLabels: false,
+          unselectedItemColor: Colors.grey.shade700,
+          elevation: 10,
+          type: BottomNavigationBarType.fixed,
+        ),
+      ),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const AuthPage(),
+        'main': (context) => const MainLayout(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: const <Widget>[
+            Text(
+              'Welcome to Boek-een-Doc, a Doctor Appointment Platform',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
